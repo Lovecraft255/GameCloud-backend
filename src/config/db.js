7e2e9867c00f9f431e131cac948976af6b0f9079
Lovecraft255 <dantegaleano194@gmail.com>
2026-01-25 15:53:52 -0300

    Conexion modificada

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // 👈 CLAVE PARA RENDER
     },
   },
 });
